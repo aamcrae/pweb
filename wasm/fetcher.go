f6a9e0e6274149c1a9aedfe83ee5e6187f0daa84
Andrew McRae <amcrae@google.com>
2024-06-10 11:10:37 +1000

    wasm: Add some comments

--- wasm/fetcher.go
+++ wasm/fetcher.go
@@ -7,7 +7,8 @@ import (
 	"syscall/js"
 )
 
-// Fetcher is an interface to the JS fetch API
+// Fetcher is an interface to the JS fetch API.
+// It is suitable for use with tinygo.
 type Fetcher struct {
 	w *Window
 	wg sync.WaitGroup
@@ -18,6 +19,8 @@ type Fetcher struct {
 	readyF js.Func
 }
 
+// NewFetcher returns a new instance of a fetcher.
+// The JS fetch api is called to start the reading of the file.
 func NewFetcher(w *Window, url string) * Fetcher{
 	f := &Fetcher{w: w}
 	fPromise := w.window.Call("fetch", js.ValueOf(url))
@@ -29,6 +32,8 @@ func NewFetcher(w *Window, url string) * Fetcher{
 	return f
 }
 
+// Get returns the data retrieved, or an error if the file
+// could not be accessed.
 func (f *Fetcher) Get() ([]byte, error) {
 	f.wg.Wait()
 	f.errF.Release()
@@ -46,10 +51,12 @@ func (f *Fetcher) reject(this js.Value, args []js.Value) any {
 func (f *Fetcher) response(this js.Value, args []js.Value) any {
 	ok := args[0].Get("ok")
 	if js.ValueOf(ok).Bool() == true {
+		// Accessing the data is done through a second promise.
 		p2 := args[0].Call("text")
 		p2.Call("then", f.readyF, f.errF)
 		return p2
 	}
+	// The file could not be accessed.
 	f.err = fmt.Errorf("Resp: %s", js.ValueOf(args[0].Get("status")).String())
 	f.wg.Done()
 	return nil
